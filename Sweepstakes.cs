5872fae8a1b34f00311d567953569bccd3655e12
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-09-01 16:33:16 -0700

    Finished writing UserInterface for user input.

--- Sweepstakes.cs
+++ Sweepstakes.cs
@@ -17,13 +17,20 @@ namespace SweepstakesProj
         //Constructor
         public Sweepstakes(string name)
         {
-
+            this.Name = name;
         }
 
         //Member Methods
 	    public void RegisterContestant(Contestant contestant)
         {
-
+            contestant.FirstName = UserInterface.ContestantFirstName();
+            UserInterface.ContestantFirstName();
+            contestant.LastName = UserInterface.ContestantLastName();
+            UI.ContestantLastName();
+            contestant.EmailAddress = UI.ContestantEmail();
+            UI.ContestantEmail();
+            contestant.RegistrationNumber = UI.ContestantRegistrationNumber();
+            UI.ContestantRegistrationNumber();
         }
 	    public Contestant PickWinner()
         {
--- UserInterface.cs
+++ UserInterface.cs
@@ -1,22 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SweepstakesProj
 {
-    class UserInterface
+    public static class UserInterface
     {
         //Member Variables
-
+        public static int contestantNumber;
 
         //Constructor
-
+        static UserInterface()
+        {
+            contestantNumber = 0;
+        }
 
         //Member Methods
-        static public string GetUserInputFor(string prompt)
+        public static string ContestantFirstName()
         {
+            Console.WriteLine("Enter your first name here: ");
+            return Console.ReadLine();
+        }
+
+        public static string ContestantLastName()
+        {
+            Console.WriteLine("Enter your last name here: ");
+            return Console.ReadLine();
+        }
+
+        public static string ContestantEmail()
+        {
+            Console.WriteLine("Enter your e-mail address here:");
+            return Console.ReadLine();
+        }
+
+        public static int ContestantRegistrationNumber()
+        {
+            contestantNumber = +1;
+            Console.WriteLine("Your registration number is " + contestantNumber);
+            return contestantNumber;
+        }
+
+        public static string ManagerType()
+        {
+            bool type = false;
+            while (!type)
+            {
+                Console.WriteLine("How would you like to manage the Sweepstakes: stack or queue?");
+                string choice = Console.ReadLine();
+
+                if (choice == "stack" || choice == "queue")
+                {
+                    type = true;
+                    return choice;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong Input please try again. Type {0}stack{0} or {0}queue{0}.", Convert.ToChar(34));
+                    type = false;
+                }
+            }
             return null;
         }
     }
