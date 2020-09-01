20108294cf42bb88d465144a4e20010828fa4524
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-09-01 14:21:48 -0700

    Instatiated member variables in Constestants class and wrote in member methods.

--- Contestant.cs
+++ Contestant.cs
@@ -9,17 +9,24 @@ namespace SweepstakesProj
     class Contestant
     {
         //Member Variables
-        public string FirstName;
-        public string LastName;
-        public string EmailAddress;
-        public int RegistrationNumber;
+        public string firstName;
+        public string lastName;
+        public string emailAddress;
+        public int registrationNumber;
 
         //Constructor
-
-
+        public Contestant(string firstName, string lastName, string emailAddress)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+        }
 
         //Member Methods
-
+        public string FirstName { get { return firstName; } set { firstName = value; } }
+        public string LastName { get { return lastName; } set { lastName = value; } }
+        public string EmailAddress { get { return emailAddress; } set { emailAddress = value; } }
+        public int RegistrationNumber { get { return registrationNumber; } set { registrationNumber = value; } }
 
 
     }
